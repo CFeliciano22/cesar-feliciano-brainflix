d65650391dc8f2690cee00eada242bf252643a63
Cesar Feliciano <cesarfeliciano1@aol.com>
2022-03-07 11:32:49 -0500

    no changes made to main just saving doc

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,50 +5,7 @@ import VideoList from "../VideoList/VideoList";
 import Comments from "../Comments/Comments";
 import './Main.scss'
 import Form from "../Form/Form";
-// import axios from "axios";
 
-// const Url ='https://project-2-api.herokuapp.com/videos/';
-// const ApiKey = '?api_key=cb37a238-0bd0-4f02-9855-6ae33c69f64d/';
-
-//     class Main extends React.Component {
-//         state = {
-//           Vids: [],
-//           currentVideo: {},
-//           comments:[]
-//         }
-        
-//         getVideoById = (id) => {
-//             axios
-//                 .get(`${Url}${id}${ApiKey}`)
-//                 .then(response => {
-//                     console.log(response.data)
-//                     this.setState({
-//                         currentVideo: response.data,
-//                         comments:response.data.comments
-                        
-//                     })
-//                 })
-//                 .catch(err => console.log(err))
-//         }
-
-//         componentDidMount(){
-//         axios.get(Url + ApiKey).then ((response)=>{
-//             console.log(response.data)
-//             this.setState({
-//                 Vids:response.data
-//             })
-//             const videoId =  this.props.match.params.id ||  response.data[0].id 
-//             this.getVideoById(videoId);
-//         })
-//         }
-
-// componentDidUpdate(prevProps,prevState){
-//     const videoId =  this.props.match.params.id || this.state.Vids[0].id 
-//     console.log(videoId)
-//     if (prevState.currentVideo && prevState.currentVideo.id !== videoId){
-//         this.getVideoById(videoId)
-//     }
-// }
 function Main({currentVideo, Vids, comments}){
     
     return(
